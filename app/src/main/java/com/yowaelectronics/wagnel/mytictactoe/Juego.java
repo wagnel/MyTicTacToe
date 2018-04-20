fe8199dee522a209032a98a5017f9421a388d276
Wagnel <wagnelcuello@gmail.com>
2018-04-20 16:46:22 -0400

    Primer guardado

--- app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/Juego.java
+++ app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/Juego.java
@@ -2,6 +2,8 @@ package com.yowaelectronics.wagnel.mytictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Juego extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Juego extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+    }
+
+    // Este metodo recibe el cuadro que es clickable
+    public void onGame(View view) {
+        // Implement Observer Pattern
+        Toast.makeText(this,"Me dieron: ["+view.getId()+"]",Toast.LENGTH_SHORT).show();
     }
 }
