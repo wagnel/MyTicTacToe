b6b4d707dc8a807f390d3a784684492917d72e91
Wagnel <wagnelcuello@gmail.com>
2018-04-21 09:46:19 -0400

    Arreglos en el radio button - se agrego un radio group debajo

--- app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/Juego.java
+++ app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/Juego.java
@@ -1,21 +1,99 @@
 package com.yowaelectronics.wagnel.mytictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class Juego extends AppCompatActivity {
+    private String order = null;
+    private String ficha = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+
+        Intent receivedIntent = getIntent();
+        Bundle ref = receivedIntent.getBundleExtra("reference");
+        ref.getString("order");
+        ref.getString("ficha");
     }
 
     // Este metodo recibe el cuadro que es clickable
     public void onGame(View view) {
         // Implement Observer Pattern
-        Toast.makeText(this,"Me dieron: ["+view.getId()+"]",Toast.LENGTH_SHORT).show();
+        ImageButton num1 = findViewById(R.id.num1);
+        ImageButton num2 = findViewById(R.id.num2);
+        ImageButton num3 = findViewById(R.id.num3);
+
+        ImageButton num4 = findViewById(R.id.num4);
+        ImageButton num5 = findViewById(R.id.num5);
+        ImageButton num6 = findViewById(R.id.num6);
+
+        ImageButton num7 = findViewById(R.id.num7);
+        ImageButton num8 = findViewById(R.id.num8);
+        ImageButton num9 = findViewById(R.id.num9);
+
+        num1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [1]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [2]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [3]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //------------------------------------------------------------------------------------------//
+        num4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [4]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [5]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [6]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        //------------------------------------------------------------------------------------------//
+        num7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [7]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [8]",Toast.LENGTH_SHORT).show();
+            }
+        });
+        num9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Me dieron [9]",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/MainActivity.java
+++ app/src/main/java/com/yowaelectronics/wagnel/mytictactoe/MainActivity.java
@@ -17,16 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onRadioBtnClick(View view) {
-        RadioButton circle = findViewById(R.id.radioBtnCircle);
-        RadioButton cruz = findViewById(R.id.radioBtnCruz);
-
-        if (circle.isChecked())
-            cruz.setEnabled(false);
-        else if (cruz.isChecked())
-            circle.setEnabled(false);
-    }
-
     public void playGame(View view) {
         RadioButton circle = findViewById(R.id.radioBtnCircle);
         RadioButton cruz = findViewById(R.id.radioBtnCruz);
@@ -35,13 +25,19 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Debe seleccionar una ficha",Toast.LENGTH_SHORT).show();
 
         }else{
-
+            Bundle b = new Bundle();
             CheckBox first = findViewById(R.id.chkFirst);
 
-            if (!first.isChecked())
-                Toast.makeText(getApplicationContext(),"Eres segundo en jugar",Toast.LENGTH_SHORT).show();
+            if (!first.isChecked()) {
+                b.putString("order","2");
+                Toast.makeText(getApplicationContext(), "Eres segundo en jugar", Toast.LENGTH_SHORT).show();
+            }
+
+            String ficha = (circle.isChecked()) ? "circle" : "cruz";
+            b.putString("ficha",ficha);
 
             Intent intent = new Intent(this,Juego.class);
+            intent.putExtra("reference",b);
             startActivity(intent);
         }
     }
